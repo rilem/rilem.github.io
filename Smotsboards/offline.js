8b5c3a7f8562b1cff7520eabe32c03c0b07c0c9b
Richard <richard@q42.nl>
2018-03-21 15:52:16 +0100

    smotsboards

---
+++ Smotsboards/offline.js
@@ -0,0 +1,1 @@
+{"version":1521643425656,"fileList":["c2runtime.js","data.js","start.js","style.css","offlineClient.js","images/whitepanel-sheet0.png","images/tiledbackground-sheet0.png","images/shared-0-sheet0.png","icons/loading-logo.png","icons/icon-64.png","icons/icon-32.png","icons/icon-256.png","icons/icon-128.png","icons/icon-512.png","icons/icon-16.png","register-sw.js"]}
